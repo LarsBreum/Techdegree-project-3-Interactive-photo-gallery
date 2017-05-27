4669b7d91e257c3a65aa62fd14ff2fb86c4df269
Lars Breum Hansen <lars.breum.lh@gmail.com>
2017-05-27 08:50:23 +0200

    started js

--- js/main.js
+++ js/main.js
@@ -7,4 +7,20 @@ lightbox.option({
 
 });
 
-//Search the images
+function searchFunction() {
+	//declare vars
+	var input, divs, filter, dataTitle;
+	input = $('input[type=search]');
+	//case insensitivity
+	filter = input.val().toLowerCase();
+	//Get the data-title values
+	var dataTitleVal = $('a[data-title]').attr("data-title").toLowerCase();
+	console.log(dataTitleVal[1]);
+	//output the field value
+	console.log(filter);
+
+	/*loop through all a's to see if filter is included in dataTitleVal
+	display: none, if it is not */
+
+
+}
