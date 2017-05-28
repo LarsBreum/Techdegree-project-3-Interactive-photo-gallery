9e674c4e11c1328288798a3055e7d8acb3d0775f
Lars Breum Hansen <lars.breum.lh@gmail.com>
2017-05-28 07:18:50 +0200

    made search part work
    Hiding the element when the filter is not a part of the caption

--- js/main.js
+++ js/main.js
@@ -14,20 +14,22 @@ function searchFunction() {
 	var allAnchors = $('a[data-lightbox=gallery');
 	//Get number of a tags on page
 	var numOfAnchors = allAnchors.length;
-	//case insensitivity
+	//case insensitivity by setting the filter to lowercase
 	filter = input.val().toLowerCase();
 
 
 	//Loop through all 12 images
 	for (var i = 0; i < numOfAnchors; i++ ) {
-		//get attribute data-title
+		/*get attribute data-title (to search by the caption) and setting the
+		caption to lower case*/
 		var dataTitleVal = 
 		document.getElementsByTagName('a')[i].getAttribute("data-title").toLowerCase();
 		//if filter is not a substr of dataTitleVal hide the anchor
 		if (dataTitleVal.indexOf(filter) == -1) {
-			allAnchors[i].css('display', 'none');
+			$(allAnchors[i]).hide('500');
+		//if it is, show the anchor
 		} else {
-			allAnchors[i].css('display', 'initial');
+			$(allAnchors[i]).show('500');
 		}
 	}
 }
